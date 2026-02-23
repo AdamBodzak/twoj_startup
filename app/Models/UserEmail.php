b1dce761b86f21567c7ef553e2974900ab2fb240
adambodzak <adam.bodzak@bprog.pl>
2026-02-23 19:42:04 +0100

    add relation to user and user Email

--- app/Models/UserEmail.php
+++ app/Models/UserEmail.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserEmail extends Model
 {
-    //
+    protected $casts = ['is_primary' => 'bool'];
+
+    /**
+     * Relation to user
+     *
+     * @return BelongsTo
+     */
+    public function userRelation(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
